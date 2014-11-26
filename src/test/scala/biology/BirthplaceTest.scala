e7ed5c03612511079276b2220e99e0e3af6a9311
shawes <steven.hawes@sydney.edu.au>
2014-11-27 00:11:28 +1100

    Added unit tests for birthplace

--- src/test/scala/biology/BirthplaceTest.scala
+++ src/test/scala/biology/BirthplaceTest.scala
@@ -1,7 +1,24 @@
 package biology
 
 import org.scalatest.FlatSpec
+import physical.GeoCoordinate
 
-class BirthPlaceTest extends FlatSpec {
+class BirthplaceTest extends FlatSpec {
+
+  val birthplace = new Birthplace("hobart", new GeoCoordinate(1, 2))
+
+  "A birthplace" should "not be null upon construction" in {
+    assert(birthplace != null)
+  }
+
+  it should "have the name passed to the constructor" in {
+    assert(birthplace.name == "hobart")
+  }
+
+  it should "have the coordinate passed to the constructor" in {
+    assert(birthplace.location.latitude == 1)
+    assert(birthplace.location.longitude == 2)
+  }
+
 
 }
