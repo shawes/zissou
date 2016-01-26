61aac910e5c6681e3adf8446363281627b61c7e5
shawes <steven.hawes@sydney.edu.au>
2016-01-26 23:55:39 +1100

    Added Turbulence Tests

--- src/main/scala/physical/Turbulence.scala
+++ src/main/scala/physical/Turbulence.scala
@@ -1,16 +1,15 @@
 package physical
 
-import com.github.nscala_time.time.Imports._
 import org.apache.commons.math3.random.MersenneTwister
 
 import scala.compat.Platform
 
 class Turbulence(val horizontalDiffusionCoefficient: Double, val verticalDiffusionCoefficient: Double) {
-  val random = new MersenneTwister(Platform.currentTime)
+  var random = new MersenneTwister(Platform.currentTime)
 
   def this() = this(0, 0)
 
-  def apply(v: Velocity, t: Duration) : Unit = {
+  def apply(v: Velocity): Velocity = {
     val horizontalTurbulence = horizontalDiffusionCoefficient * random.nextDouble()
     val verticalTurbulence = verticalDiffusionCoefficient * random.nextDouble()
     new Velocity(v.u + horizontalTurbulence, v.v + horizontalTurbulence, v.w + verticalTurbulence)
