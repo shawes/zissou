4816b4914e7761a7b5dfc882f7cd570bd152aaa5
stevehawes <maxhawes@gmail.com>
2016-02-01 23:02:35 +1100

    Added new empty tests

---
+++ src/test/scala/physical/GeoCoordinateTest.scala
@@ -0,0 +1,8 @@
+package physical
+
+import org.scalatest.mock.MockitoSugar
+import org.scalatest.{FlatSpec, PrivateMethodTester}
+
+class GeoCoordinateTest extends FlatSpec with MockitoSugar with PrivateMethodTester {
+
+}
