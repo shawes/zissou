4c003efba7278cab9d4ba8a78009c7f22b7d7ab0
Steven Hawes <maxhawes@gmail.com>
2017-09-08 12:26:35 +0800

    Removed adding the random component to the Turbulence, its now a static object (hard to unit test)

--- src/test/scala/physical/TurbulenceTest.scala
+++ src/test/scala/physical/TurbulenceTest.scala
@@ -15,53 +15,53 @@ class TurbulenceTest extends FlatSpec with MockitoSugar with PrivateMethodTester
   }
 
   it should "initialise the correct horizontal turbulence" in {
-    val mockRandom = mock[Random]
-    when(mockRandom.get).thenReturn(4)
-    val turbulence = new Turbulence(1.0, 2.0, 3, mockRandom)
+    //val mockRandom = mock[Random]
+    //when(mockRandom.get).thenReturn(4)
+    val turbulence = new Turbulence(1.0, 2.0, 3)
     val h_turb_cal: Double = Math.pow((2.0 * 1.0) / 3, 0.5)
     turbulence.horizontalTurbulence should equal(h_turb_cal)
   }
 
   it should "initialise the correct vertical turbulence" in {
-    val mockRandom = mock[Random]
-    when(mockRandom.get).thenReturn(4)
-    val turbulence = new Turbulence(1.0, 2.0, 3, mockRandom)
+    //val mockRandom = mock[Random]
+    //when(mockRandom.get).thenReturn(4)
+    val turbulence = new Turbulence(1.0, 2.0, 3)
     val v_turb_cal = Math.pow((2.0 * 2.0) / 3, 0.5)
     turbulence.verticalTurbulence should equal(v_turb_cal)
   }
 
-  it should "apply the correct vertical turbulence" in {
-    val mockRandom = mock[Random]
-    when(mockRandom.get).thenReturn(0.2)
-    val turbulence = new Turbulence(1.0, 2.0, 3, mockRandom)
-    val v_turb_cal = Math.pow((2.0 * 2.0) / 3, 0.5) * 0.2
-    val velocity = new Velocity(5.0, 7.0, 9.0)
-    val result = turbulence.apply(velocity)
-    result.w should equal(velocity.w + v_turb_cal)
-  }
-
-  it should "apply the correct horizontal turbulence" in {
-    val mockRandom = mock[Random]
-    when(mockRandom.get).thenReturn(0.2)
-    val turbulence = new Turbulence(1.0, 2.0, 3, mockRandom)
-    val h_turb_cal = Math.pow((2.0 * 1.0) / 3, 0.5) * 0.2
-    val velocity = new Velocity(5.0, 7.0, 9.0)
-    val result = turbulence.apply(velocity)
-    result.u should equal(velocity.u + h_turb_cal)
-    result.v should equal(velocity.v + h_turb_cal)
-  }
-
-
-  it should "apply equal vertical & horizontal coefficients" in {
-    val mockRandom = mock[Random]
-    when(mockRandom.get).thenReturn(1)
-    val turbulence = new Turbulence(2, 2, 1, mockRandom)
-    val velocity = new Velocity(1, 2, 3)
-    val result = turbulence.apply(velocity)
-
-    assert(result.u == 3)
-    assert(result.v == 4)
-    assert(result.w == 5)
-  }
+  // it should "apply the correct vertical turbulence" in {
+  //   val mockRandom = mock[Random]
+  //   when(mockRandom.get).thenReturn(0.2)
+  //   val turbulence = new Turbulence(1.0, 2.0, 3)
+  //   val v_turb_cal = Math.pow((2.0 * 2.0) / 3, 0.5) * 0.2
+  //   val velocity = new Velocity(5.0, 7.0, 9.0)
+  //   val result = turbulence.apply(velocity)
+  //   result.w should not equal(3)
+  // }
+  //
+  // it should "apply the correct horizontal turbulence" in {
+  //   val mockRandom = mock[Random]
+  //   when(mockRandom.get).thenReturn(0.2)
+  //   val turbulence = new Turbulence(1.0, 2.0, 3)
+  //   val h_turb_cal = Math.pow((2.0 * 1.0) / 3, 0.5) * 0.2
+  //   val velocity = new Velocity(5.0, 7.0, 9.0)
+  //   val result = turbulence.apply(velocity)
+  //   result.u should equal(velocity.u + h_turb_cal)
+  //   result.v should equal(velocity.v + h_turb_cal)
+  // }
+  //
+  //
+  // it should "apply equal vertical & horizontal coefficients" in {
+  //   val mockRandom = mock[Random]
+  //   when(mockRandom.get).thenReturn(1)
+  //   val turbulence = new Turbulence(2, 2, 1)
+  //   val velocity = new Velocity(1, 2, 3)
+  //   val result = turbulence.apply(velocity)
+  //
+  //   assert(result.u == 3)
+  //   assert(result.v == 4)
+  //   assert(result.w == 5)
+  // }
 
 }
